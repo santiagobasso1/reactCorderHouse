09ee5491e0b19cd582f02bd4a88c8f45ce5c1cfd
Santiago Basso <santiagobasso@hotmail.com>
2022-12-05 20:33:41 -0300

    PreEntrega 2 Basso Santiago

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -10,25 +10,8 @@ const ItemListContainer = () => {
 
     useEffect(() => {
         if (categoria){
-            let idCategoriaSeleccionada;
-            //Utilizo un switch porque es la forma que se me ocurrió para que en el link aparezca el nombre de la categoria y no el numero
-            switch (categoria) {
-                case "procesadores":
-                    idCategoriaSeleccionada=1;
-                    break;
-
-                case "graficas":
-                    idCategoriaSeleccionada=2; 
-                    break;
-                case "motherboards":
-                    idCategoriaSeleccionada=3;       
-                    break;
-                default:
-                    idCategoriaSeleccionada=0;
-                    break;
-            }
             consultarBDD('../json/productos.json').then(products => {
-            const productList  = products.filter(prod => prod.stock > 0).filter(prod=> prod.idCategoria===parseInt(idCategoriaSeleccionada));
+            const productList  = products.filter(prod => prod.stock > 0).filter(prod=> prod.idCategoria===categoria);
             const cardProductos = ItemList({productList});
             setProductos(cardProductos);
             });
