2e093f7d73b444e68eea45f9d056cf3744a6850a
Santiago Basso <santiagobasso@hotmail.com>
2022-12-08 19:34:36 -0300

    Arreglando Icono Carrito

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,12 +1,7 @@
 import { Link } from "react-router-dom";
 const CartWidget = () => {
     return (
-        <ul className='navbar-nav me-auto'>
-            <li className='nav-link'> 
-                <button className='btn btn-dark'><Link className='nav-link' to={"/cart"}>Carrito</Link></button>
-            </li>
-            <p>0</p>
-        </ul>
+                <button className='btn btn-dark'><Link className='nav-link' to={"/cart"}><i className="fas fa-shopping-cart fa-lg"></i><span className="contadorCarrito">0</span></Link></button>
         );
 }
 
